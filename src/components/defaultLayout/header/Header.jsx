4812d07c7787792c882ac0360a934cbd819dd8b5
LevaTiger <bognar.levente99@gmail.com>
2025-03-27 15:31:24 +0100

    Homepage First version

--- src/components/defaultLayout/header/Header.jsx
+++ src/components/defaultLayout/header/Header.jsx
@@ -1,12 +1,9 @@
 import { Link, NavLink } from "react-router";
 import './header.css'
+import { useState } from "react";
 const Header=()=>{
 
-    const selectedPage =()=>{
-        const currentPath = window.location.href;
-        console.log(`Current Path is: ${currentPath}`)
-    }
-    selectedPage();
+    const [menuVisible, setMenuVisible] = useState(false);
 
     return(
         <div className="header-content">
@@ -16,16 +13,17 @@ const Header=()=>{
                     </figure>
                 <h2>Parfüm Oázis</h2>
             </div>
-            <nav>
+            <div className="hamburger" onClick={()=> setMenuVisible(!menuVisible)}>
+                ☰
+            </div>
+            <nav className={menuVisible? "open" : ""}>
                 <ul>
                     <li>
                         <NavLink to={'/'} 
                             className={({ isActive }) => (isActive ? 'active' : '')}>Főoldal
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink to={'/bejelentkezes'} className={({ isActive }) => (isActive ? 'active' : '')}>Bejelentkezés</NavLink>
-                    </li>
+                    
                     <li>
                         <NavLink to={'/ferfi-parfumok'} className={({ isActive }) => (isActive ? 'active' : '')}>Férfi Parfümök</NavLink>
                     </li>
@@ -39,7 +37,18 @@ const Header=()=>{
                         <NavLink to={'/rolunk'} className={({ isActive }) => (isActive ? 'active' : '')}>Rólunk</NavLink>
                     </li>
                     <li>
-                        <NavLink to={'/kosar'} className={({ isActive }) => (isActive ? 'active' : '')}>Kosár</NavLink>
+                        <NavLink to={'/kosar'} className={({ isActive }) => (isActive ? 'active' : '')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                            </svg>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/bejelentkezes'} className={({ isActive }) => (isActive ? 'active' : '')}>
+                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                            </svg>
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,14 +1,31 @@
+import './homepage.css'
+
 const Homepage=()=>{
 
-    const divepic={
-        width:'1200px',
-        height:'700px',
-        backgroundColor:'blue'
-    }
     return(
         <div className="container">
-            <div style={divepic}>
-                asd
+            <div className="home-layout">
+                <h2 className='primary-gradient'>KELETI PARFÜMÖK MAGYAR KERESKEDŐJE</h2>
+                <figure>
+                    <img src="https://www.parfumoazis.hu/wp-content/uploads/2024/10/image1-e1729533035134.jpeg" alt="hero-img" />
+                </figure>
+
+                <h2 className="primary-gradient">NÉZD MEG TERMÉKEINKET</h2>
+
+                <div className="parfume-types">
+                    <div>
+                        <img src="https://www.parfumoazis.hu/wp-content/uploads/2024/11/ferfi1.jpg" alt="" />
+                        <button>Féri Parfümök</button>
+                    </div>
+                    <div>
+                        <img src="https://www.parfumoazis.hu/wp-content/uploads/2024/11/noi1.jpg" alt="" />
+                        <button>Női Parfümök</button>
+                    </div>
+                    <div>
+                        <img src="https://www.parfumoazis.hu/wp-content/uploads/2024/11/892750C9-EED7-499F-94BB-E4B101F05FF41.png" alt="" />
+                        <button>Unisex Parfümök</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
